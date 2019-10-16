4cce4b68bbcd795a93c97520f032353925eca978
gihonglee123 <48290638+gihonglee123@users.noreply.github.com>
2019-10-16 16:48:24 -0500

    Add files via upload

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class main {
+    public static void main(String[] args){
+        TXTReceiptBuilder txt = new TXTReceiptBuilder();
+       // txt.appendPurchase(purchase);
+        System.out.println(txt.sb);
+        System.out.println("heyyy");
+    }
+}
